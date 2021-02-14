fdc7d99a2329c401ab8730d60d65c9f186c741a8
Sebastian <sebastian.neumair@gmail.com>
2021-02-14 13:32:15 +0100

    Add Capacitor

--- projects/game-engine-lib/src/lib/game-engine-lib.module.ts
+++ projects/game-engine-lib/src/lib/game-engine-lib.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedLibModule } from '@shared-lib';
 import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [BoardComponent],
-  imports: [CommonModule
+  imports: [CommonModule, SharedLibModule
   ],
   exports: [BoardComponent]
 })
